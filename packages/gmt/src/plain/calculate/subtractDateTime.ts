0718cd777c31872be5ec7c9d9cf3564738aebde5
Craig C. <craigocurtis@gmail.com>
2026-03-19 13:27:12 +0200

    Update packages/gmt/src/plain/calculate/subtractDateTime.ts
    Co-authored-by: Baldur Páll Hólmgeirsson <baldurpan@gmail.com>

--- packages/gmt/src/plain/calculate/subtractDateTime.ts
+++ packages/gmt/src/plain/calculate/subtractDateTime.ts
@@ -17,26 +17,5 @@ export function subtractDateTime(
 
   const dateTime = Temporal.PlainDateTime.from(value);
 
-  switch (unit) {
-    case "year":
-      return dateTime.subtract({ years: amount }).toString();
-    case "month":
-      return dateTime.subtract({ months: amount }).toString();
-    case "week":
-      return dateTime.subtract({ weeks: amount }).toString();
-    case "day":
-      return dateTime.subtract({ days: amount }).toString();
-    case "hour":
-      return dateTime.subtract({ hours: amount }).toString();
-    case "minute":
-      return dateTime.subtract({ minutes: amount }).toString();
-    case "second":
-      return dateTime.subtract({ seconds: amount }).toString();
-    case "millisecond":
-      return dateTime.subtract({ milliseconds: amount }).toString();
-    case "microsecond":
-      return dateTime.subtract({ microseconds: amount }).toString();
-    case "nanosecond":
-      return dateTime.subtract({ nanoseconds: amount }).toString();
-  }
+  return dateTime.subtract({ [unit]: amount }).toString();
 }
