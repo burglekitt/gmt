5249d737a2c7aa6336c41e662494ad14422f3028
Craig C. <craigocurtis@gmail.com>
2026-03-19 13:27:47 +0200

    Update packages/gmt/src/zoned/calculate/addZoned.ts
    Co-authored-by: Baldur Páll Hólmgeirsson <baldurpan@gmail.com>

--- packages/gmt/src/zoned/calculate/addZoned.ts
+++ packages/gmt/src/zoned/calculate/addZoned.ts
@@ -26,28 +26,8 @@ export function addZoned(
   let zoned: Temporal.ZonedDateTime;
   try {
     zoned = Temporal.ZonedDateTime.from(value);
+    return zoned.add({ [unit]: amount })?.toString() || "";
   } catch {
     return "";
-  }
-
-  switch (unit) {
-    case "year":
-      return zoned.add({ years: amount }).toString();
-    case "month":
-      return zoned.add({ months: amount }).toString();
-    case "week":
-      return zoned.add({ weeks: amount }).toString();
-    case "day":
-      return zoned.add({ days: amount }).toString();
-    case "hour":
-      return zoned.add({ hours: amount }).toString();
-    case "minute":
-      return zoned.add({ minutes: amount }).toString();
-    case "second":
-      return zoned.add({ seconds: amount }).toString();
-    case "millisecond":
-      return zoned.add({ milliseconds: amount }).toString();
-    default:
-      return "";
   }
 }
