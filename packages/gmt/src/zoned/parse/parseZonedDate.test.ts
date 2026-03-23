4d354c9971a54dc489125801f6c781a9bd702322
Craig C. <craigocurtis@gmail.com>
2026-03-23 09:08:14 +0200

    chore(gmt): remove duplicate tests parseZonedDate

--- packages/gmt/src/zoned/parse/parseZonedDate.test.ts
+++ packages/gmt/src/zoned/parse/parseZonedDate.test.ts
@@ -63,32 +63,3 @@ describe("parseZonedDate", () => {
     });
   }
 });
-
-describe("parseZonedDate battle-test timezones (local noon 2024-02-29T12:00)", () => {
-  it.each`
-    value                                               | expected
-    ${"2024-02-29T12:00:00+00:00[UTC]"}                 | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+00:00[GMT]"}                 | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+00:00[Etc/GMT]"}             | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+00:00[Europe/Lisbon]"}       | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+00:00[Europe/Dublin]"}       | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+01:00[Europe/Berlin]"}       | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+02:00[Europe/Helsinki]"}     | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+03:00[Europe/Istanbul]"}     | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+05:30[Asia/Kolkata]"}        | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+05:45[Asia/Kathmandu]"}      | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+08:00[Asia/Shanghai]"}       | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+11:00[Australia/Lord_Howe]"} | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+13:45[Pacific/Chatham]"}     | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00+13:00[Pacific/Apia]"}        | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00-11:00[Pacific/Niue]"}        | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00-05:00[America/New_York]"}    | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00-06:00[America/Chicago]"}     | ${"2024-02-29"}
-    ${"2024-02-29T12:00:00-07:00[America/Phoenix]"}     | ${"2024-02-29"}
-  `(
-    "returns $expected for local noon in battle-test $value",
-    ({ value, expected }: { value: string; expected: string }) => {
-      expect(parseZonedDate(value)).toBe(expected);
-    },
-  );
-});
