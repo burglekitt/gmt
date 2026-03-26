ee6a4c2a397fc39047eb04ad10ea3b017bcb5a2b
Craig C. <craigocurtis@gmail.com>
2026-03-26 11:31:08 +0200

    chore(gmt): remove .ts extensions in barrel file

--- packages/gmt/src/types/index.ts
+++ packages/gmt/src/types/index.ts
@@ -1,4 +1,4 @@
-export * from "./date-duration-unit.ts";
+export * from "./date-duration-unit";
 export * from "./date-time-duration-units";
 export * from "./fractional-digit";
-export * from "./time-duration-unit.ts";
+export * from "./time-duration-unit";
