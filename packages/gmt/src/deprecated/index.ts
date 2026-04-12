430f2c1873bc7380c1a3adfccc7c108277e9550e
Craig C. <craigocurtis@gmail.com>
2026-04-12 19:37:29 +0300

    Fix deprecated barrel file

--- packages/gmt/src/deprecated/index.ts
+++ packages/gmt/src/deprecated/index.ts
@@ -1,5 +1,6 @@
 // convertUnixToTimezone - same as convertUnixToZoned, but with the system timeZone
 // isUtcDateTime - same as isValidUtc, but with a different name and no leap second support
-export * from "./convertUnixToTimezone";
+export * from "./convertTimezoneToUtc";
 export * from "./convertUnixToTimezone";
 export * from "./isUtcDateTime";
+export * from "./isValidDateOrDateTime";
